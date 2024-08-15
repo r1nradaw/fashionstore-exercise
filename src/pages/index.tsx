bad2a0aae09131c790bff4c81de63b6cddfacd13
Rinrada Wattanachanasorn <rinradawattanachanasorn@Rinradas-MacBook-Air.local>
2024-08-15 20:14:02 +0700

    hw 2 finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,16 @@
 import ProductList from "@/components/product/ProductList";
 import { useCart } from "@/contexts/cart";
 import { Category, Product } from "@/models/product";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const fetchProducts = (): Promise<Product[]> => {
-  const products = [
-    {
-      id: "91479834-ac10-42c4-87c0-4584f9083851",
-      name: "Loose Cropped Jeans (Damaged)",
-      price: 42.57,
-      imageUrl:
-        "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/448429/item/goods_66_448429.jpg?width=300",
-      category: Category.WOMEN,
-    },
-    {
-      id: "91479834-ac10-42c4-87c0-4584f9083857",
-      name: "Smart Skort Solid",
-      price: 140.37,
-      imageUrl:
-        "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/455844/item/goods_09_455844.jpg?width=300",
-      category: Category.WOMEN,
-    },
-    {
-      id: "a8ce731c-f553-46d5-9ff6-563d198f3e39",
-      name: "Oxford Striped Slim Fit Long Sleeve Shirt",
-      price: 28.28,
-      imageUrl:
-        "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/452300/sub/goods_452300_sub14.jpg?width=1600&impolicy=quality_75",
-      category: Category.MEN,
-    },
-  ];
-  return new Promise((resolve) => resolve(products));
+  const getUrl = "https://fashion-store.fly.dev/products";
+  const newProducts = axios.get(getUrl).then((response) => {
+    return response.data.data as Product[];
+  });
+
+  return new Promise((resolve) => resolve(newProducts));
 };
 
 export default function Home() {
@@ -46,6 +26,7 @@ export default function Home() {
     let isStale = false;
     setIsLoading(true);
     fetchProducts().then((productsResult) => {
+      console.log(productsResult)
       if (!isStale) {
         setIsLoading(false);
         setProducts(productsResult);
